9e10b35d6d9241ff01de53b112ebcc6300903b79
tolam <tolam0902@gmail.com>
2024-06-25 21:52:29 +0900

    baitap

---
+++ src/main/java/frontend/Program03.java
@@ -0,0 +1,11 @@
+package frontend;
+
+import backend.Exercise03;
+import entity.Animal;
+
+public class Program03 {
+    public static void main(String[] args) {
+//        Integer[] array = {1, 2, 3, 4};
+        Exercise03.question04(new Integer[] {1,2,3,4});
+    }
+}
---
+++ src/main/java/frontend/Program04.java
@@ -0,0 +1,9 @@
+package frontend;
+
+import backend.Exercise04;
+
+public class Program04 {
+    public static void main(String[] args) {
+        System.out.println("Exercise04.max(2,4) = " + Exercise04.max(2, 4));
+    }
+}
